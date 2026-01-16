4184b7b2571a986ccad3b3670e51553952e3a71f
Aaron Banse <acbanse@icloud.com>
2026-01-16 15:33:25 -0800

    Clean up main and restructure for new compute architecture. TODO: write non-patched downsampler for image->input surface

--- src/main.zig
+++ src/main.zig
@@ -3,12 +3,11 @@ const math = std.math;
 const posix = std.posix;
 const heap = std.heap;
 
-const patch = @import("image_patch.zig");
-const debug = @import("debug.zig");
 const glyph = @import("glyph.zig");
 const algo = @import("algo.zig");
 const uni_im = @import("unicode_image.zig");
 const term = @import("terminal_util.zig");
+const compute = @import("compute.zig");
 
 const c = @cImport({
     @cInclude("stb_image.h");
@@ -19,12 +18,43 @@ pub fn main() !void {
     defer _ = debug_allocator.deinit();
     const allocator = debug_allocator.allocator();
 
-    // load image
+    // setup dimensions
+    const term_dims = term.getDims();
+    const w = 4; // patch width
+    const h = 4; // patch height
+
+    // get codepoints
+    const u8_vals: u16 = math.maxInt(u8) + 1; // num vals u8 can take
+    var codepoints: [u8_vals*2]u32 = undefined;
+    for (0..u8_vals) |n| {
+        codepoints[n] = 0x2800 + @as(u16, @intCast(n));
+        if (n == 0x91 or n == 0x92 or n == 0x93) {
+            codepoints[n+u8_vals] = 0x2500;
+        } else {
+            codepoints[n+u8_vals] = 0x2500 + @as(u16, @intCast(n));
+        }
+    }
+
+    // precompute glyph set cache
+    var glyph_set_cache: algo.GlyphSetCache(w,h) = undefined;
+    try glyph_set_cache.init(&codepoints, allocator);
+    defer glyph_set_cache.deinit(allocator);
+
+    // load image from disk
     var img_w: u32 = undefined;
     var img_h: u32 = undefined;
     var img_chan_n: u32 = undefined;
-    const image_raw: [*]u8 = c.stbi_load("/home/acbanse/Projects/fidelitty/.img/IMG_3706.JPEG", @ptrCast(&img_w), @ptrCast(&img_h), @ptrCast(&img_chan_n), 3);
+    const image_raw: [*]u8 = c.stbi_load("/home/acbanse/Projects/fidelitty/.img/IMG_3706.JPEG",
+        @ptrCast(&img_w), @ptrCast(&img_h), @ptrCast(&img_chan_n), 3);
     defer c.stbi_image_free(image_raw);
+
+    // initialize compute context
+    var compute_context: compute.Context = undefined;
+    try compute_context.init(allocator, w, h, glyph_set_cache, 8);
+    defer compute_context.deinit();
+
+    // create a render pipeline
+    const pipeline_handle = try compute_context.createRenderPipeline(32, 32);
 
     // TEMP: will delete----------
     const image_flipped: []u8 = try allocator.alloc(u8, img_w * img_h * 3);
@@ -40,62 +70,30 @@ pub fn main() !void {
     }
     // END TEMP-------------------
 
-    // setup dimensions
-    const term_dims = term.getDims();
-    const w = 4; // patch width
-    const h = 4; // patch height
+    // TODO: read image into pipelin handle input surface
+
+    // run pipeline
+    try compute_context.executeRenderPipelines(&.{pipeline_handle});
+
+    // wait on completion
+    try compute_context.waitRenderPipelines(&.{pipeline_handle});
+
+    // Init output image to fill terminal
+    var out_image: uni_im.UnicodeImage = undefined;
     const out_image_h: u16 = term_dims.rows;
     const out_image_w: u16 = @intFromFloat(@as(f32, @floatFromInt(term_dims.rows * term_dims.cell_h)) // new_h
         * (@as(f32, @floatFromInt(img_w)) / @as(f32, @floatFromInt(img_h))) / @as(f32, @floatFromInt(term_dims.cell_w))); // old_w / old_h
-
-    // init output image
-    var out_image: uni_im.UnicodeImage = undefined;
     try out_image.init(allocator, 0, 0, out_image_w, out_image_h);
     defer out_image.deinit(allocator);
-
-    // sample patches
-    var patches: []patch.ImagePatch(w,h) = try allocator.alloc(patch.ImagePatch(w,h), out_image_w * out_image_h);
-    defer allocator.free(patches);
-    for (0..out_image_h) |y| {
-        for (0..out_image_w) |x| {
-            patches[y * out_image_w + x].sample(image_flipped, @intCast(img_w), @intCast(img_h),
-                out_image_w, out_image_h, @intCast(x), @intCast(y));
-        }
-    }
-
-    // get codepoints
-    const u8_vals: u16 = math.maxInt(u8) + 1; // num vals u8 can take
-    var codepoints: [u8_vals*2]u32 = undefined;
-    for (0..u8_vals) |n| {
-        codepoints[n] = 0x2800 + @as(u16, @intCast(n));
-        if (n == 0x91 or n == 0x92 or n == 0x93) {
-            codepoints[n+u8_vals] = 0x2500;
-        } else {
-            codepoints[n+u8_vals] = 0x2500 + @as(u16, @intCast(n));
-        }
-    }
-
-    // get pixmaps
-    const glyph_mask_generator: glyph.GlyphMaskGenerator = try .init(allocator, "Adwaita/AdwaitaMono-Regular.ttf");
-    defer glyph_mask_generator.deinit(allocator);
-
-    const glyphs = try glyph.getGlyphMaskSet(w, h, &codepoints, &glyph_mask_generator, allocator);
-    defer allocator.free(glyphs);
-
-    // precompute glyph color solvers
-    var solvers: [codepoints.len]algo.GlyphColorSolver(w,h) = undefined;
-    for (0..codepoints.len) |n| {
-        solvers[n] = algo.glyphColorSolver(w, h, glyphs[n]);
-    }
 
     // write pixels to image
     for (0..out_image_h) |y| {
         for (0..out_image_w) |x| {
-            const pixel = algo.computePixel(w, h, patches[y * out_image_w + x], &codepoints, glyphs, &solvers);
-            out_image.writePixel(pixel, @intCast(x), @intCast(y));
+            out_image.writePixel(pipeline_handle.output_surface[y * out_image_w + x], @intCast(x), @intCast(y));
         }
     }
 
+    // print image
     _ = try posix.write(1, out_image.buf);
 }
 
